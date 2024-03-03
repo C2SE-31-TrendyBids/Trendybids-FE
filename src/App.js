870ecd90bdc43511fd94d7ea0a2e080bbaceb025
tanlee <lexuantan159@gmail.com>
2024-03-03 15:46:54 +0700

    Initial: set up config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {router} from "./routes/index";
+import {Default} from "./layouts/index";
+import React, {Fragment} from "react";
+import {NotFound} from "./pages/index"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            {router.map((route, index) => {
+                const Page = route.component;
+
+                let LayoutDynamic = Default;
+                if (route.layout) LayoutDynamic = route.layout;
+                else if (route.layout === null) LayoutDynamic = Fragment;
+
+                return (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        element={
+                            <LayoutDynamic>
+                                <Page/>
+                            </LayoutDynamic>
+                        }
+                    />
+                );
+            })}
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import {
+    Home
+} from '../pages/index';
+
+const router = [
+    {path: '/', component: Home},
+];
+
+
+export {router}
