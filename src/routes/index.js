0d93705afa874b63fcae7e461040fd3d858415e2
Thien Quang <toiquang24@gmail.com>
2024-03-07 08:14:47 +0700

    Update new

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,19 @@ import {
 
 import {
     Admin,
-    User
+    User,
+    NoHeaderAndFooter
 } from '../layouts/index';
 
 
 const router = [
     { path: '/', component: Home },
-    { path: '/login', layout: User, component: Login },
-    { path: '/register', layout: User, component: Register },
-    { path: '/forgot-password', layout: User, component: ForgotPassword },
-    { path: '/reset-password', layout: User, component: ResetPassword },
+    { path: '/login', layout: NoHeaderAndFooter, component: Login },
+    { path: '/register', layout: NoHeaderAndFooter, component: Register },
+    { path: '/forgot-password', layout: NoHeaderAndFooter, component: ForgotPassword },
+    { path: '/reset-password', layout: NoHeaderAndFooter, component: ResetPassword },
     { path: '/profile', layout: User, component: Profile },
     { path: '/admin', layout: Admin, component: AdminPage },
-
-
 
 ]
 
