0dbe97b7843c2add04ff4d44f76d1e3e5775ddba
Quang Quoc <quangquoc1542002@gmail.com>
2024-03-27 21:26:39 +0700

    Feat: Add map in Contact Page

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import ReactMapGL, {FullscreenControl, GeolocateControl, Marker, NavigationControl} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Map = () => {
+    const [viewState, setViewState] = useState({
+        latitude: 16.059942,
+        longitude: 108.209742,
+        zoom: 17
+    })
+    return (
+        <ReactMapGL {...viewState}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+            transitionDuration='200'
+            mapStyle="mapbox://styles/quangquoc154/clo4dm2y300og01pf4o3mg3pm"
+            onMove={(evt) => setViewState(evt.viewState)}
+        >
+            <Marker longitude="108.209742" latitude="16.059942"/>
+            <GeolocateControl/>
+            <FullscreenControl/>
+            <NavigationControl/>
+        </ReactMapGL>
+    );
+};
+
+export default Map;
